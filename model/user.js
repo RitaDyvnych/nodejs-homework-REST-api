4e9453a5f5b99047581b5d15bebdeb474cef8eec
RitaDyvnych <m.dyvnych@gmail.com>
2022-01-14 14:39:28 +0200

    частично работает, сохраняет аватар в нужную папку из tmp

--- model/user.js
+++ model/user.js
@@ -1,5 +1,6 @@
 import pkg from 'mongoose'
 import bcrypt from 'bcryptjs'
+import gravatar from 'gravatar'
 const { Schema, model } = pkg
 
 const userSchema = new Schema(  {
@@ -29,6 +30,12 @@ const userSchema = new Schema(  {
       type: String,
       default: null,
     },
+    avatar: {
+      type: String,
+      default: function () {
+        return gravatar.url(this.email, { s: '250' }, true) // public/avatar/avatar.jpg
+      },
+    }
 },
 {
     versionKey: false,
